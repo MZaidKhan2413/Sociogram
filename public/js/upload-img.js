c61bb96ab5dbca1b1a94cbf11d5cf01d0eee9762
Mohammad Zaid Khan <mzaidk222@gmail.com>
2023-11-30 19:11:31 +0530

    added some features

--- public/js/upload-img.js
+++ public/js/upload-img.js
@@ -1,6 +1,15 @@
+// Upload new post
 let srcImage = document.getElementById('source-img');
 let inputImage = document.getElementById('image');
 
 inputImage.onchange = function(){
     srcImage.src = URL.createObjectURL(inputImage.files[0]);
+}
+
+// Edit user profile
+let srcImageEdit = document.getElementById('source-edit-img');
+let inputImageEdit = document.getElementById('edit-image');
+
+inputImageEdit.onchange = function(){
+    srcImageEdit.src = URL.createObjectURL(inputImageEdit.files[0]);
 }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({storage});
 const User = require("../models/users.js");
 const wrapAsync = require("../utils/WrapAsync.js");
+const {isLoggedIn} = require("../utils/Middlewares.js");
 
 // Show user profile
 router.get("/:uid", wrapAsync( async (req, res) => {
   let uid = req.params.uid;
   let user = await User.findById(uid).populate({path: "posts", options: {sort: {_id: -1}}});
   res.render("posts/user-profile.ejs", { user });
+}));
+
+// edit user profile
+router.get("/:uid/edit", isLoggedIn, wrapAsync( async (req, res)=>{
+  let uid = req.params.uid;
+  let user = await User.findById(uid);
+  res.render("user/user-edit.ejs", {user});
+}));
+router.put("/:uid", upload.single("profile_pic"), wrapAsync( async (req, res)=>{
+  let uid = req.params.uid;
+  let bio = req.body.bio;
+  let updatedUser = await User.findByIdAndUpdate(uid, {bio});
+  if (typeof req.file !== "undefined") {
+    let profile_pic = req.file.path;
+    updatedUser.profile_pic = {profile_pic};
+    await updatedUser.save();
+  }
+  req.flash('success', "Profile updated successfully");
+  res.redirect(`/user/${uid}`);
 }));
 
 // show a specific post of user
